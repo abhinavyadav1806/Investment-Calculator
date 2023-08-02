7d2bdde0cd94773f107be5803ee5405f7b62d69c
abhinavyadav1806 <abhinav1806yadav@gmail.com>
2023-08-02 21:53:17 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import logo_image from '../../assets/investment-calculator-logo.png';
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={logo_image} alt="logo" />
+      <h1>*Investment Calculator*</h1>
+    </header>
+  );
+}
+
+// export default Header;
